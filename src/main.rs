bd96ae3c0c5665bd194692129c156ffff644ff23
Alec Graham <alec.p.graham@protonmail.com>
2025-04-26 02:45:01 -0400

    grep in a file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 
 
@@ -7,18 +9,42 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
 
-    let query = &args[1];
-    let fp = &args[2];
+    // let query = &args[1];
+    // let fp = &args[2];
 
-    println!("searching query {}", query);
-    println!("In file: {}", fp);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(fp)
-        .expect(" error, should have been a file");
+    println!("searching query {}", config.query);
+    println!("In file: {}", config.file_path);
 
+    let _ = run(config);
+
+}
+
+struct Config {
+    query: String,
+    file_path: String, 
+}
+
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config,  &'static str> {
+        if args.len() < 3 {
+            return Err("this is an error");
+        }
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Ok(Config { query, file_path })
+    }  
+}
+
+
+fn run(config : Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
     println!("contents:\n {}", contents);
-
-
-    // fn parse_config();
-
+    Ok(())
 }
